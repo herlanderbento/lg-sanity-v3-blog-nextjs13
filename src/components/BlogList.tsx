f415b28e33f383ad861bb44a6562b3060dad3344
Herlander Bento <herlanderbento19@gmail.com>
2023-02-01 21:59:31 +0100

    commit: config setup for building and components

--- src/components/BlogList.tsx
+++ src/components/BlogList.tsx
@@ -13,12 +13,12 @@ export default function BlogList({ posts }: Props) {
       <hr className='border-[#f7ab0a] mb-10' />
       <div className='grid grid-cols-1 md:grid-cols-2 gap-y-16 pb-24  px-10 gap-10'>
         {posts.map((post) => (
-          <ClientSideRoute key={post._id} route={`/post/${post.slug.current}`}>
+          <ClientSideRoute key={post._id} route={`/post/${post?.slug?.current}`}>
             <div  className='flex flex-col group cursor-pointer'>
               <div className='relative w-full h-80 drop-shadow-xl group-hover:scale-105 transition-transform duration-200 case-out'>
                 <Image
                   className='object-cover object-left lg:object-center'
-                  src={urlFor(post.mainImage).url()}
+                  src={urlFor(post?.mainImage).url()}
                   alt='image'
                   fill
                 />
